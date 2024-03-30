97c0f9c3e1d7eee79ba124320e508b833cd94c2d
Sathish-1810 <163336167+Sathish-1810@users.noreply.github.com>
2024-03-30 12:27:26 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,10 @@
+
+function switchOff(){
+    document.getElementById("bulbimage").src=("off.jpeg");
+    document.getElementById("ghost").src=("horror.jpg");
+    
+}
+function switchOn(){
+    document.getElementById("bulbimage").src=("on.jpeg");
+    document.getElementById("ghost").src=("woman.jpeg");
+}
